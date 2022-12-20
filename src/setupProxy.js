16a4558469a597c5135983617962656ddb918b8e
PhYell <94902895+PhYell@users.noreply.github.com>
2022-12-20 21:29:43 +0400

    changed url to api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         "/api",
         createProxyMiddleware({
-            target: "http://localhost:5000",
+            target: "https://movie-backend-7vfd.onrender.com/api",
             changeOrigin: true,
         })
     )
